0ba9423caefa6ce8c7b95e9e74a46f492fca99f0
Rohit Kumar Pandey <rohitpandey@Rohits-MacBook-Air.local>
2023-08-27 20:33:42 +0530

    fixed reports.py and added pdf generation part in main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,9 +3,35 @@
 import reports
 import emails
 import os
+from datetime import datetime
+
 
 def main():
-    
+    description_file_path = ''
+    os.chdir(description_file_path)
+    description = []
+    for infile in os.listdir(description_file_path):
+        with open(infile, 'r') as file:
+            data = file.split('\n')
+            dict = {}
+            dict["name"] = data[0]
+            dict["weight"] = data[1]
+            description.append(dict)
+        
+    #This is the part where we need to generate report
+    pdf_body = ""
+    current_datetime = datetime.now()
+    formated_date = current_datetime.strftime("%B %d, %Y")
+    title = "Processed Update on {}".format(formated_date)
+    reports.generate("processed.pdf",title,pdf_body)
+
+
+    #This is where we will send mail.
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    sender = "automation@example.com"
+    reciver = "username@example.com"
+    Subject = "Upload Completed - Online Fruit Store"
+    message = emails.generate(sender,reciver,Subject,body,"")
     
 
 
--- scripts/reports.py
+++ scripts/reports.py
@@ -1,14 +1,15 @@
 #!/opt/homebrew/bin/python3
 
-##/usr/bin/env python3
-#importing libraries
-
 import reportlab
 
 from reportlab.platypus import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.platypus import Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
 
-def generate_report():
-    pass
+def generate(filename, title, additional_info):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(additional_info, styles["BodyText"])
+  empty_line = Spacer(1,20)
+  report.build([report_title, empty_line, report_info, empty_line])
